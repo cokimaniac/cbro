62b3bce14ac556bfcfec6c438ae3ed753a1e925d
Jorge Luis <cokimaniac@gmail.com>
2020-12-31 15:24:25 -0400

    password property added to the User schema.

--- api/users/model.js
+++ api/users/model.js
@@ -15,6 +15,10 @@ let userSchema = new mongoose.Schema({
     email: {
         type: String,
         required: true
+    },
+    password: {
+        type: String,
+        required: true
     }
 });
 
