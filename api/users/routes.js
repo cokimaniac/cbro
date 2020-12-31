a3451ccd092fd55ea6ead07c281f356c3a3303ce
Jorge Luis <cokimaniac@gmail.com>
2020-12-31 16:29:42 -0400

    /users/login and /users/profile routes added.

--- api/users/routes.js
+++ api/users/routes.js
@@ -1,12 +1,19 @@
 const express = require("express");
 //modules
 const controller = require("./controller");
+const { verifyToken } = require("../../network/middleware");
 // Route instance
 const router = express.Router();
 
-router.get("/", controller.list);
+// requires authorization token
+router.get("/", verifyToken, controller.list);
+router.get("/profile", verifyToken, controller.profile);
+router.get("/:userID", verifyToken, controller.retrieve);
+router.delete("/:userID", verifyToken, controller.delete);
+
+// public routes
 router.post("/signup", controller.create);
-router.get("/:userID", controller.retrieve);
-router.delete("/:userID", controller.delete);
+router.post("/login", controller.login);
+
 
 module.exports = router;
