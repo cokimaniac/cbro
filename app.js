e27a63cdce81f2f1dfdda320437e512162358176
Jorge Luis <cokimania@outlook.com>
2021-01-14 00:09:11 -0400

    cors module added

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const bodyParser = require("body-parser");
 const express = require("express");
+const cors = require("cors");
+// modules
 const userRoutes = require("./api/users/routes");
 const debtorRoutes = require("./api/debtors/routes");
 
 // App Instance
 const app = express();
+
+// CORS
+app.use(cors());
 
 // Body Parser
 app.use(bodyParser.urlencoded({ extended: false }));
