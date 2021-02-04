4f2cffc6298057f38f8328370acc3b251c423179
Jorge Luis <cokimaniac@gmail.com>
2021-02-04 01:39:31 -0400

    socket io dependency added

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 const bodyParser = require("body-parser");
 const express = require("express");
 const cors = require("cors");
+const io = require("socket.io");
 // modules
 const userRoutes = require("./api/users/routes");
 const debtorRoutes = require("./api/debtors/routes");
+const testRoutes = require("./api/tests/routes");
 
 // App Instance
 const app = express();
 
 // CORS
 app.use(cors());
+
+// socket.io
+app.set("socketio", io);
 
 // Body Parser
 app.use(bodyParser.urlencoded({ extended: false }));
--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const http = require("http");
+const socketIo = require("socket.io");
+
 require("dotenv").config();
 // modules
 const app = require("./app");
@@ -11,4 +13,40 @@ const config = {
 };
 
 db();
+
+const io = socketIo(server, {   
+    cors: {
+        origin: "http://localhost:3000"
+    }
+});
+
+io.on("connect", (socket) => {
+    socket.emit("test", getInfo());
+});
+
+const getInfo = () => {
+    return [
+        {
+            name: "Jorge Luis",
+            age: 29
+        },
+        {
+            name: "Lucas Jorge",
+            age: 3
+        },
+        {
+            name: "Marisela",
+            age: 29
+        },
+        {
+            name: "Jorge Dalmiro",
+            age: 64
+        },
+        {
+            name: "Nancy Gladys",
+            age: 62
+        }
+    ];
+}
+
 server.listen(config.port, () => console.log(`Server running at ${config.host}:${config.port}`));
